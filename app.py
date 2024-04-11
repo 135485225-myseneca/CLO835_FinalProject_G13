6c3f5da727177caa3f4f34f46ffbef80b9fbe256
153974225-myseneca <156478933+153974225-myseneca@users.noreply.github.com>
2024-04-11 01:22:00 -0400

    Update app.py - removes background color code and adds background image code

--- app.py
+++ app.py
@@ -2,57 +2,68 @@ from flask import Flask, render_template, request
 from pymysql import connections
 import os
 import random
-import argparse
+import boto3
+from botocore.exceptions import NoCredentialsError
 
-
+# Initialize Flask app
 app = Flask(__name__)
 
+# AWS S3 configuration
+AWS_BUCKET_NAME = 'project-clo835'
+AWS_REGION_NAME = 'us-east-1'
 DBHOST = os.environ.get("DBHOST") or "localhost"
 DBUSER = os.environ.get("DBUSER") or "root"
-DBPWD = os.environ.get("DBPWD") or "passwors"
+DBPWD = os.environ.get("DBPWD") or "password"
 DATABASE = os.environ.get("DATABASE") or "employees"
-COLOR_FROM_ENV = os.environ.get('APP_COLOR') or "lime"
 DBPORT = int(os.environ.get("DBPORT"))
 
 # Create a connection to the MySQL database
 db_conn = connections.Connection(
-    host= DBHOST,
+    host=DBHOST,
     port=DBPORT,
-    user= DBUSER,
-    password= DBPWD, 
-    db= DATABASE
-    
+    user=DBUSER,
+    password=DBPWD, 
+    db=DATABASE
 )
-output = {}
-table = 'employee';
 
-# Define the supported color codes
-color_codes = {
-    "red": "#e74c3c",
-    "green": "#16a085",
-    "blue": "#89CFF0",
-    "blue2": "#30336b",
-    "pink": "#f4c2c2",
-    "darkblue": "#130f40",
-    "lime": "#C1FF9C",
-}
+# Initialize AWS S3 client
+s3 = boto3.client('s3', region_name=AWS_REGION_NAME)
 
+# Function to download a random image from S3 bucket
+def download_random_image_from_s3(bucket_name):
+    try:
+        # List objects in the bucket
+        response = s3.list_objects_v2(Bucket=bucket_name)
+        # Extract the list of image keys
+        image_keys = [obj['Key'] for obj in response.get('Contents', [])]
+        # Choose a random image key
+        random_image_key = random.choice(image_keys)
+        return random_image_key
+    except NoCredentialsError:
+        print("Credentials not available.")
 
-# Create a string of supported colors
-SUPPORTED_COLORS = ",".join(color_codes.keys())
+# Specify local path to store the downloaded image
+BACKGROUND_IMAGE_PATH = 'static/background.jpeg'
 
-# Generate a random color
-COLOR = random.choice(["red", "green", "blue", "blue2", "darkblue", "pink", "lime"])
+# Download a random image from S3 bucket
+def download_background_image():
+    random_image_key = download_random_image_from_s3(AWS_BUCKET_NAME)
+    if random_image_key:
+        s3.download_file(AWS_BUCKET_NAME, random_image_key, BACKGROUND_IMAGE_PATH)
 
-
+# Route for home page
 @app.route("/", methods=['GET', 'POST'])
 def home():
-    return render_template('addemp.html', color=color_codes[COLOR])
+    download_background_image()  # Download background image on each request to ensure freshness
+    return render_template('addemp.html', background_image=BACKGROUND_IMAGE_PATH)
 
+# Route for about page
 @app.route("/about", methods=['GET','POST'])
 def about():
-    return render_template('about.html', color=color_codes[COLOR])
-    
+    download_background_image()  # Download background image on each request to ensure freshness
+    return render_template('about.html', background_image=BACKGROUND_IMAGE_PATH)
+
+# AddEmp route
 @app.route("/addemp", methods=['POST'])
 def AddEmp():
     emp_id = request.form['emp_id']
@@ -60,41 +71,39 @@ def AddEmp():
     last_name = request.form['last_name']
     primary_skill = request.form['primary_skill']
     location = request.form['location']
-
   
     insert_sql = "INSERT INTO employee VALUES (%s, %s, %s, %s, %s)"
     cursor = db_conn.cursor()
 
     try:
-        
-        cursor.execute(insert_sql,(emp_id, first_name, last_name, primary_skill, location))
+        cursor.execute(insert_sql, (emp_id, first_name, last_name, primary_skill, location))
         db_conn.commit()
         emp_name = "" + first_name + " " + last_name
-
     finally:
         cursor.close()
 
-    print("all modification done...")
-    return render_template('addempoutput.html', name=emp_name, color=color_codes[COLOR])
+    print("All modifications done...")
+    return render_template('addempoutput.html', name=emp_name, background_image=BACKGROUND_IMAGE_PATH)
 
+# GetEmp route
 @app.route("/getemp", methods=['GET', 'POST'])
 def GetEmp():
-    return render_template("getemp.html", color=color_codes[COLOR])
+    return render_template("getemp.html", background_image=BACKGROUND_IMAGE_PATH)
 
-
+# FetchData route
 @app.route("/fetchdata", methods=['GET','POST'])
 def FetchData():
     emp_id = request.form['emp_id']
-
     output = {}
+
     select_sql = "SELECT emp_id, first_name, last_name, primary_skill, location from employee where emp_id=%s"
     cursor = db_conn.cursor()
 
     try:
-        cursor.execute(select_sql,(emp_id))
+        cursor.execute(select_sql, (emp_id))
         result = cursor.fetchone()
         
-        # Add No Employee found form
+         # Add No Employee found form
         output["emp_id"] = result[0]
         output["first_name"] = result[1]
         output["last_name"] = result[2]
@@ -103,34 +112,11 @@ def FetchData():
         
     except Exception as e:
         print(e)
-
     finally:
         cursor.close()
 
-    return render_template("getempoutput.html", id=output["emp_id"], fname=output["first_name"],
-                           lname=output["last_name"], interest=output["primary_skills"], location=output["location"], color=color_codes[COLOR])
+    return render_template("getempoutput.html", id=output["emp_id"], fname=output["first_name"],lname=output["last_name"], interest=output["primary_skills"], location=output["location"], background_image=BACKGROUND_IMAGE_PATH)
 
 if __name__ == '__main__':
-    
-    # Check for Command Line Parameters for color
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--color', required=False)
-    args = parser.parse_args()
-
-    if args.color:
-        print("Color from command line argument =" + args.color)
-        COLOR = args.color
-        if COLOR_FROM_ENV:
-            print("A color was set through environment variable -" + COLOR_FROM_ENV + ". However, color from command line argument takes precendence.")
-    elif COLOR_FROM_ENV:
-        print("No Command line argument. Color from environment variable =" + COLOR_FROM_ENV)
-        COLOR = COLOR_FROM_ENV
-    else:
-        print("No command line argument or environment variable. Picking a Random Color =" + COLOR)
-
-    # Check if input color is a supported one
-    if COLOR not in color_codes:
-        print("Color not supported. Received '" + COLOR + "' expected one of " + SUPPORTED_COLORS)
-        exit(1)
-
-    app.run(host='0.0.0.0',port=8080,debug=True)
+    # Run Flask app
+    app.run(host='0.0.0.0', port=8080, debug=True)
